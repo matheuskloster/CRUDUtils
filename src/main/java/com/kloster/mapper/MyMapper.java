654b726b2c7f73a9a403c3e16078f0238b38780e
matheuskloster <matheuskloster@users.noreply.github.com>
2024-03-03 10:39:29 -0300

    Optimizing MyMapper

--- src/main/java/com/kloster/mapper/MyMapper.java
+++ src/main/java/com/kloster/mapper/MyMapper.java
@@ -3,27 +3,16 @@ package com.kloster.mapper;
 import com.kloster.entity.MyEntity;
 import com.kloster.model.MyModel;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 import org.springframework.stereotype.Component;
 
-
 @Component
-@Mapper(componentModel = "spring", imports = {MyModel.class, MyEntity.class})
+@Mapper(componentModel = "spring")
 public interface MyMapper {
 
     MyMapper INSTANCE = Mappers.getMapper(MyMapper.class);
 
-    @Mapping(source= "id", target = "id")
-    @Mapping(source= "nome", target = "nome")
-    @Mapping(source= "idade", target = "idade")
     MyEntity toMyEntity(MyModel myModel);
 
-    @Mapping(source= "id", target = "id")
-    @Mapping(source= "nome", target = "nome")
-    @Mapping(source= "idade", target = "idade")
     MyModel toMyModel(MyEntity myEntity);
-
-
-
 }
