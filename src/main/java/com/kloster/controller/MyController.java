642e597bb56d8a7f8ec927d623652e3a15cf1968
matheuskloster <matheuskloster@users.noreply.github.com>
2024-03-03 09:40:47 -0300

    Adding docker-compose.yml, exercises from mbaas

--- src/main/java/com/kloster/controller/MyController.java
+++ src/main/java/com/kloster/controller/MyController.java
@@ -2,17 +2,20 @@ package com.kloster.controller;
 
 import com.kloster.model.MyModel;
 import com.kloster.service.MyService;
+import io.swagger.annotations.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api")
 @RequiredArgsConstructor
+@Api(value = "MyController", description = "Operações disponíveis em MyController")
 public class MyController {
 
     private final MyService service;
 
     @GetMapping()
+    @ApiOperation(value = "Retorna um MyModel", response = MyModel.class)
     public MyModel myEndpoint() {
         MyModel myModel = new MyModel();
         myModel.setIdade(service.getIdade());
@@ -21,12 +24,28 @@ public class MyController {
     }
 
     @PostMapping("/save")
-    public MyModel mySaveEndpoint(@RequestBody MyModel myModel) {
+    @ApiOperation(value = "Salva um MyModel e retorna o objeto salvo", response = MyModel.class)
+    public MyModel mySaveEndpoint(@ApiParam(value = "Objeto MyModel que precisa ser salvo", required = true) @RequestBody MyModel myModel) {
         return service.saveModel(myModel);
     }
 
-    @GetMapping("/get")
-    public MyModel myGetByNomeEndpoint(@RequestParam String nome) {
+    @GetMapping("/get/{id}")
+    @ApiOperation(value = "Retorna um MyModel com base no ID fornecido", response = MyModel.class)
+    @ApiResponses(value = {
+        @ApiResponse(code = 200, message = "Objeto MyModel encontrado"),
+        @ApiResponse(code = 404, message = "Não foi encontrado nenhum objeto MyModel com o ID fornecido")
+    })
+    public MyModel myGetByIdEndpoint(@ApiParam(value = "ID do objeto MyModel que precisa ser obtido", required = true) @PathVariable Long id) {
+        return service.findById(id);
+    }
+
+    @GetMapping("/get/name")
+    @ApiOperation(value = "Retorna um MyModel com base no nome fornecido", response = MyModel.class)
+    @ApiResponses(value = {
+        @ApiResponse(code = 200, message = "Objeto MyModel encontrado"),
+        @ApiResponse(code = 404, message = "Não foi encontrado nenhum objeto MyModel com o nome fornecido")
+    })
+    public MyModel myGetByNomeEndpoint(@ApiParam(value = "Nome do objeto MyModel que precisa ser obtido", required = true) @RequestParam String nome) {
         return service.findByNome(nome);
     }
 }
--- src/main/java/com/kloster/service/MyService.java
+++ src/main/java/com/kloster/service/MyService.java
@@ -1,7 +1,6 @@
 package com.kloster.service;
 
 import com.kloster.mapper.MyMapper;
-import com.kloster.mapper.MyMapperImpl;
 import com.kloster.model.MyModel;
 import com.kloster.repository.MyRepository;
 import lombok.RequiredArgsConstructor;
@@ -29,5 +28,9 @@ public class MyService {
 
     public MyModel findByNome(String nome) {
         return mapper.toMyModel(repository.findByNome(nome));
+    }
+
+    public MyModel findById(Long id) {
+        return mapper.toMyModel(repository.findById(String.valueOf(id)).orElse(null));
     }
 }
